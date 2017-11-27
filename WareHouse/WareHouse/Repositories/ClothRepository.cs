2707e011ea9f5acab2c5eff233e395a972162ab8
Albert Bach <bach_albert@hotmail.hu>
2017-11-27 17:30:42 +0100

    Summary added

--- WareHouse/WareHouse/Repositories/ClothRepository.cs
+++ WareHouse/WareHouse/Repositories/ClothRepository.cs
@@ -17,7 +17,21 @@ namespace WareHouse.Repositories
         {
             return ClothContext.Clothes.ToList();
         }
-        
+
+        public List<Cloth> Filters(string name)
+        {
+            return ClothContext.Clothes.Where(c => c.ItemName.Contains(name)).ToList();
+        }
+
+        public List<Cloth> FilterSize(string size)
+        {
+            return ClothContext.Clothes.Where(c => c.Size.Contains(size)).ToList();
+        }
+
+        public List<Cloth> FilterSummary ()
+        {
+            return ClothContext.Clothes.ToList();
+        }
         public List<Cloth> FilterLower(double price)
         {
             return ClothContext.Clothes.Where(c => c.UnitPrice < price).ToList();
